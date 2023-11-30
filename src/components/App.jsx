36574e64161d5c6dc919b1ac52e703e17dcf80e0
Nadiia17 <nababiy@gmail.com>
2023-11-30 17:06:28 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { Quizlist } from './Quizlist/Quizlist';
 import initialQuizItems from '../data.json';
 import { SearchBar } from './SearchBar/SearchBar';
+import { QuizForm } from './QuizForm/QuizForm';
 
 export class App extends Component {
   state = {
@@ -10,6 +11,7 @@ export class App extends Component {
   render() {
     return (
       <div>
+        <QuizForm />
         <SearchBar />
         <Quizlist items={this.state.quizItems} />
       </div>
